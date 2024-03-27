4b158e61320aed0945ebe9f96dc01a02341bbfb5
ceeygithub <cynthiaekhomwandolor@gmail.com>
2024-03-27 16:13:21 +0100

    styling

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -9,7 +9,7 @@ const Admin = () => {
     navigate('/');
   };
   return (
-    <div className='App'>
+    <div className='' style={{display:'flex',flexDirection:'column', justifyContent:'center',alignItems:'center',marginTop:'10rem',textDecoration:'none',color:'white'}}>
       <div style={{textAlign:'center'}}>Admin page</div>
    <a href="https://docs.google.com/spreadsheets/d/1GnrVlsarLzp0FZ397mefmbRcHOPn1Z17y4PQQXXre9M/edit?usp=sharing" className='link' target="_blank" rel="noopener noreferrer"  style={{marginTop:'10rem',textDecoration:'none',color:'white'}}>Go to Google Sheets to see responses</a>
  <button onClick={handleLogout}>Logout</button>
