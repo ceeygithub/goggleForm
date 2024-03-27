19a6a8af95fd25bc7089e86a6bd062ea7f917528
ceeygithub <cynthiaekhomwandolor@gmail.com>
2024-03-27 16:09:29 +0100

    added styling

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -9,11 +9,11 @@ const Admin = () => {
     navigate('/');
   };
   return (
-    <>
+    <div className='App'>
       <div style={{textAlign:'center'}}>Admin page</div>
-   <a href="https://docs.google.com/spreadsheets/d/1GnrVlsarLzp0FZ397mefmbRcHOPn1Z17y4PQQXXre9M/edit?usp=sharing" className='link' target="_blank" rel="noopener noreferrer"  style={{display:'flex',flexDirection:'column', justifyContent:'center',alignItems:'center',marginTop:'10rem',textDecoration:'none',color:'white'}}>Go to Google Sheets to see responses</a>
+   <a href="https://docs.google.com/spreadsheets/d/1GnrVlsarLzp0FZ397mefmbRcHOPn1Z17y4PQQXXre9M/edit?usp=sharing" className='link' target="_blank" rel="noopener noreferrer"  style={{marginTop:'10rem',textDecoration:'none',color:'white'}}>Go to Google Sheets to see responses</a>
  <button onClick={handleLogout}>Logout</button>
-    </>
+    </div>
   );
 };
 
