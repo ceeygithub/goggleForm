7dbbc2a5bcf499c6295f3e57f4ea8f235af54021
ceeygithub <cynthiaekhomwandolor@gmail.com>
2024-03-28 07:28:02 +0100

    radio and checkboxes done

--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Form.css';
-// import Response from './Response';
+
 
 const FormPage = () => {
   const [submitted, setSubmitted] = useState(false);
@@ -52,13 +52,32 @@ const FormPage = () => {
                 required
               />
             </label>
-            <label htmlFor="number" id="number-label">What is your goal?
+            <label htmlFor="entry.61730274" id="number-label">What is your goal?
               <input
                 id="number"
                 type="text"
                 name="entry.61730274"
               />
             </label>
+             <fieldset>
+            <label htmlFor='entry.520046930'>On a scale of 1 to 5, how much do you agree with the statement: "I enjoy meeting new people and socializing at events?</label>
+        
+           <label htmlFor="Strongly disagree"><input id="entry.520046930" type="radio" name="entry.520046930" value="Strongly disagree" /> Strongly disagree</label>
+              <label htmlFor="Disagree"><input id="entry.520046930" type="radio" name="entry.520046930" value="Disagree" /> Disagree</label>
+              <label htmlFor="Neutral"><input id="entry.520046930" type="radio" name="entry.520046930" value="Neutral" /> Neutral</label>
+              <label htmlFor="Agree"><input id="entry.520046930" type="radio" name="entry.520046930" value="Agree" /> Agree</label>
+              <label htmlFor="Strongly agree"><input id="entry.520046930" type="radio" name="entry.520046930" value="Strongly agree" /> Strongly agree</label>
+          </fieldset>
+
+           <fieldset>
+            <label htmlFor='entry.1502698870'>
+             A car travels at an average speed of 60 miles per hour. How far will it travel in 3 hours?
+            </label>
+          <label htmlFor="120 miles"><input id="entry.1502698870" type="checkbox" name="entry.1502698870" value="120 miles" /> 120 miles</label>
+              <label htmlFor="150 miles"><input id="entry.1502698870" type="checkbox" name="entry.1502698870" value="150 miles" /> 150 miles</label>
+              <label htmlFor="180 miles"><input id="entry.1502698870" type="checkbox" name="entry.1502698870" value="180 miles" /> 180 miles</label>
+              <label htmlFor="200 miles"><input id="entry.1502698870" type="checkbox" name="entry.1502698870" value="entry.1502698870" /> 200 miles</label>
+          </fieldset>
 
             <button type='submit' id="submit">Submit</button>
           </form>
